2e808c0c87f0ec81c9bdeb1d2e199837371705d4
Julian Hernandez Acosta <jhernandezacosta@uk-c02xj3fejhd3.uk.deloitte.com>
2019-10-15 15:03:35 +0100

    merge to jenkins

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeAppAndroid() {
+		System.out.println("Nativa App Android");
+	}
+	
+	@Test
+	public void IOSApps () {
+		System.out.println("IOS Apps");
+	}
+}
